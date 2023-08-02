d956bbefa262cde4d6e1d9274c9113bd609d5fda
Tiago França <tiagofranca.rita@gmail.com>
2023-08-02 11:19:44 -0300

    Criacao de deploy aws, config rds aws e ec2, criacao do endpoint juno api SSL

---
+++ src/main/java/br/com/franca/ShirtVirtual/config/tokens/ApiTokenAsass.java
@@ -0,0 +1,9 @@
+package br.com.franca.ShirtVirtual.config.tokens;
+
+public class ApiTokenAsass {
+
+    public static final String TOKEN_ASASS_AMBIENTE_PROD ="$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAzMzI4Mjc6OiRhYWNoX2Q4ZDVlODJhLTYxYTktNDFkYi05YzE0LTMxZGVhNGFlNDBlYw==";
+
+    public static final String TOKEN_ASASS_AMBIENTE_SANDBOX ="$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwNjA0NjI6OiRhYWNoX2YyMGI5NmQ0LWFhMmItNDk3NS1hMGE5LTZmMGJmODhiYjJhYw==";
+
+}
---
+++ src/main/java/br/com/franca/ShirtVirtual/service/AccessTokenJunoService.java
@@ -0,0 +1,31 @@
+package br.com.franca.ShirtVirtual.service;
+
+import br.com.franca.ShirtVirtual.model.AccessTokenJunoAPI;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+public class AccessTokenJunoService{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public AccessTokenJunoAPI buscaTokenAtivo() {
+
+        try {
+
+            AccessTokenJunoAPI accessTokenJunoAPI =
+                    (AccessTokenJunoAPI) entityManager
+                            .createQuery("select a from AccessTokenJunoAPI a ")
+                            .setMaxResults(1).getSingleResult();
+
+            return accessTokenJunoAPI;
+        }catch (NoResultException e) {
+            return null;
+        }
+
+
+    }
+
+}
