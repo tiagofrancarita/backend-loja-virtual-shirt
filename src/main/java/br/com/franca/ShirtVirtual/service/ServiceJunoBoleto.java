304c8202a3890f9e7a423cc81448e9b38bb6562b
Tiago França <tiagofranca.rita@gmail.com>
2023-09-20 15:53:52 -0300

    Api Checkout - End-Point Teste de Pagamento Cartão Crédito - Parte XXV

--- src/main/java/br/com/franca/ShirtVirtual/service/ServiceJunoBoleto.java
+++ src/main/java/br/com/franca/ShirtVirtual/service/ServiceJunoBoleto.java
@@ -57,6 +57,8 @@ public class ServiceJunoBoleto implements Serializable {
                 .put(ClientResponse.class);
 
         if (clientResponse.getStatus() == 204) {
+            boletoJunoRepository.deleteByCode(code);
+
             return "Cancelado com sucesso";
         }
 
