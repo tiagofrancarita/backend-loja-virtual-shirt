880f575afc9bedc1f32a7e5a4f47464af8e5ec69
Tiago França <tiagofranca.rita@gmail.com>
2023-12-05 23:32:26 -0300

    Modulo asaas -- Gravando a cobrança PIX e Boleto no banco - Parte 3

---
+++ src/main/java/br/com/franca/ShirtVirtual/utils/dto/ObjetoQrCodePixAsaasDTO.java
@@ -0,0 +1,40 @@
+package br.com.franca.ShirtVirtual.utils.dto;
+
+public class ObjetoQrCodePixAsaasDTO {
+    private String encodedImage;
+    private String payload;
+    private String expirationDate;
+    private String success;
+
+    public String getEncodedImage() {
+        return encodedImage;
+    }
+
+    public void setEncodedImage(String encodedImage) {
+        this.encodedImage = encodedImage;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
